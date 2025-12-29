284c7488b435c2001b0009a6499e28a544b8b19c
Bryan Dunk <idrumgood@gmail.com>
2025-12-29 14:54:32 -0600

    add legal

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -2,11 +2,13 @@ import { createRouter, createWebHistory } from 'vue-router';
 import WeekView from '../components/WeekView.vue';
 import UserStats from '../components/UserStats.vue';
 import AboutView from '../components/AboutView.vue';
+import LegalView from '../components/LegalView.vue';
 
 const routes = [
     { path: '/', component: WeekView },
     { path: '/stats', component: UserStats },
-    { path: '/about', component: AboutView }
+    { path: '/about', component: AboutView },
+    { path: '/legal', component: LegalView }
 ];
 
 const router = createRouter({
